e7a43fb5082c51466234328ee7075ad5ea6d9b0d
teihenn <hm.hr.yossy@gmail.com>
2025-02-24 23:47:42 +0900

    Add stdin

---
+++ 3-ioreader/stdin/go.mod
@@ -0,0 +1,3 @@
+module stdin
+
+go 1.24.0
---
+++ 3-ioreader/stdin/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	for {
+		buffer := make([]byte, 5)
+		size, err := os.Stdin.Read(buffer)
+		if err == io.EOF {
+			fmt.Println("EOF")
+			break
+		}
+
+		fmt.Printf("size: %d, input = %q\n", size, string(buffer))
+
+	}
+}
