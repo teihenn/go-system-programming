0d567da47695329fcf63294b43748d9f805224ce
teihenn <hm.hr.yossy@gmail.com>
2025-03-08 00:44:21 +0900

    Add 6-tcp-httmp/http-server

---
+++ 6-tcp-http/http-server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Server is running localhost:8080")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			fmt.Println("Accept %v\n", conn.RemoteAddr())
+			request, err := http.ReadRequest(bufio.NewReader(conn))
+			if err != nil {
+				panic(err)
+			}
+			dump, err := httputil.DumpRequest(request, true)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(dump))
+			response := http.Response{
+				StatusCode: 200,
+				ProtoMajor: 1,
+				ProtoMinor: 0,
+				Body:       io.NopCloser(strings.NewReader("Hello, World\n")),
+			}
+			response.Write(conn)
+			conn.Close()
+
+		}()
+	}
+}
